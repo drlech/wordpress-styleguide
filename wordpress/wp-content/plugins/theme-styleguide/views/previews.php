a5b692c09c6de439ba8ca451fe09c5af5e953b06
drlech <drlech@gmail.com>
2017-10-28 18:43:06 +0200

    Add titles to previews and organize them visually

--- wordpress/wp-content/plugins/theme-styleguide/views/previews.php
+++ wordpress/wp-content/plugins/theme-styleguide/views/previews.php
@@ -17,9 +17,26 @@
             'file' => $file
         ], get_bloginfo('url') . '/theme-styleguide-preview');
 
+        // Build a path to the file to display.
+        // Generally taken from the parameters passed to the view, but we don't
+        // want to display "root" (as it's not really a name of the real folder)
+        // so when on root we just display the file name.
+        $pathToShow = $path;
+        if ('root' === $pathToShow) {
+            $pathToShow = '';
+        }
+
+        $filePath = $file;
+        if ($pathToShow) {
+            $filePath = "$pathToShow/$file";
+        }
+
         ?>
 
-        <iframe src="<?php echo $url; ?>"></iframe>
+        <div class="component-preview">
+            <h3><?php echo $filePath; ?></h3>
+            <iframe src="<?php echo $url; ?>"></iframe>
+        </div>
 
         <?php
     }
